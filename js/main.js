803a11767e08594a4899aad585471a479bf1702f
urzone <github.com@urz.one>
2021-03-21 02:00:07 +0800

    Add countdown time from main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "April 10, 2020 20:00:00",
+	date: "April 18 2021, 20:08:08",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
