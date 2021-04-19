6e9f13d15f14955ea33251c77ba4b86220da1ec8
urzone <github.com@urz.one>
2021-04-19 23:19:15 +0800

    Date updating

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "April 18 2021, 20:08:08",
+	date: "May 18 2021, 20:08:08",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
